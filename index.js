dbe26a54c33c6309996b8de81f77261f218b71eb
amelkisethian <angela.melkisethian@gmail.com>
2017-10-23 00:05:32 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,35 @@
+var a = 10
+var b = 5
+
+function add(a, b) {
+return a + b
+}
+
+function subtract(a, b) {
+ return a - b
+}
+
+function multiply(a, b) {
+ return a * b
+}
+
+function divide(a, b) {
+ return a / b
+}
+
+function inc(n) {
+  return n+= 1
+}
+function dec(n) {
+  return n-= 1
+}
+
+function makeInt(n) {
+n == parseInt('n', 10)
+return n
+}
+
+function preserveDecimal(n) {
+  n == parseFloat(n, 10)
+  return n
+}
